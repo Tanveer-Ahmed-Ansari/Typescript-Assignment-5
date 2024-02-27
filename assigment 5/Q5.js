90a8956738b0e51acbe51493676b5a4c32dcea14
Tanveer-Ahmed-Ansari <159915506+Tanveer-Ahmed-Ansari@users.noreply.github.com>
2024-02-27 21:43:12 +0500

    Add files via upload

---
+++ assigment 5/Q5.js
@@ -0,0 +1,7 @@
+// Q5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// let famous_person: string = "Hazrat Ali"
+// let message: string = "Accept The Apology, Even If It Is Not Sincer"
+// console.log(`${famous_person} Once Said," ${message}"`)
+var famous_person = "Hazrat Ali";
+var massage = "Accept The Apology, Even If It Is Not Sincer";
+console.log("".concat(famous_person, " once said, ").concat(massage));
